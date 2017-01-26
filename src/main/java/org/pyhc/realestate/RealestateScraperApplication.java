26c249b7cfa9bc2cee801f83e783d43b077124da
Peter Chen <peter.chen2@mail.mcgill.ca>
2017-01-26 04:16:18 +0000

    Added home controller.

--- src/main/java/org/pyhc/realestate/RealestateScraperApplication.java
+++ src/main/java/org/pyhc/realestate/RealestateScraperApplication.java
@@ -1,9 +1,12 @@
 package org.pyhc.realestate;
 
+import org.pyhc.realestate.configuration.MainConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(MainConfiguration.class)
 public class RealestateScraperApplication {
 
     public static void main(String[] args) {
